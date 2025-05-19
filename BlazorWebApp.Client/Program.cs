b384164496fdc78767605f807ac1db81ff7be24e
stickelt <stickelt@users.noreply.github.com>
2025-05-18 22:08:12 -0700

    Fixed ambiguous reference error by removing duplicate WizardFormServiceProxy class

--- BlazorWebApp.Client/Program.cs
+++ BlazorWebApp.Client/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.Services.AddScoped<IWizardFormService, WizardFormServiceProxy>();
+builder.Services.AddScoped<IWizardFormService, BlazorWebApp.Client.Services.WizardFormServiceProxy>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
